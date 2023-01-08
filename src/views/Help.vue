8e750b09c83dffac1d850023f23c8276062b2fb4
Harut <har.amirkhanyan@gmail.com>
2023-01-08 12:34:08 +0400

    Add page help

--- src/views/Help.vue
+++ src/views/Help.vue
@@ -1,5 +1,30 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <app-page title="Help">
+    <question-item v-for="q in questions" :key="q.title" :question="q">
+    </question-item>
+  </app-page>
 </template>
+
+<script>
+import QuestionItem from '../components/question/QuestionItem.vue';
+import AppPage from '../components/ui/AppPage.vue';
+export default {
+  setup() {
+    const questions = [
+      {
+        title: 'What is in the system',
+        text: 'Here is everything for viewing applications',
+      },
+      {
+        title: 'What is in the system',
+        text: 'Here is everything for viewing applications',
+      },
+    ];
+    return {
+      questions,
+    };
+  },
+
+  components: { AppPage, QuestionItem },
+};
+</script>
