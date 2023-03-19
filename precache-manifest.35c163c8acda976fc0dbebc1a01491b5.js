110d6a971deb75a053985efe96a9d318a1850416
Harut <har.amirkhanyan@gmail.com>
2023-03-19 21:44:03 +0400

    create vue.config

---
+++ precache-manifest.35c163c8acda976fc0dbebc1a01491b5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c6e34b85218b4e9a078",
+    "url": "/vue-online-bank/css/app.bda56935.css"
+  },
+  {
+    "revision": "f871971bcb8060203f6aab6afc3b987c",
+    "url": "/vue-online-bank/index.html"
+  },
+  {
+    "revision": "7c6e34b85218b4e9a078",
+    "url": "/vue-online-bank/js/app.5d934daf.js"
+  },
+  {
+    "revision": "fcf0e30731cace8162ab",
+    "url": "/vue-online-bank/js/chunk-2d0a4783.97af5f17.js"
+  },
+  {
+    "revision": "bf3096d2dc36a26710af",
+    "url": "/vue-online-bank/js/chunk-2d0be70b.e9c8c48f.js"
+  },
+  {
+    "revision": "194c51a08a86c5a8ae2d",
+    "url": "/vue-online-bank/js/chunk-2d216dc7.aff117f9.js"
+  },
+  {
+    "revision": "2d77d471a208f1fcebd3",
+    "url": "/vue-online-bank/js/chunk-vendors.66868bfb.js"
+  },
+  {
+    "revision": "f11bce428d38fd9f4d6f0ba31c2acffc",
+    "url": "/vue-online-bank/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/vue-online-bank/robots.txt"
+  }
+]);
